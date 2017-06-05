5bb0454e679c22c8b4da69d6a2394287ecf2c940
timothyjward <timothyjward@apache.org>
2017-06-05 19:53:47 +0100

    Ensure that spec bundles can use the static factory in OSGi

---
+++ javax-persistence/2_0/src/main/java/org/apache/aries/jpa/javax/persistence/EMFBuilderServiceResolver.java
@@ -0,0 +1,103 @@
+/*  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.aries.jpa.javax.persistence;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
+import javax.persistence.spi.LoadState;
+import javax.persistence.spi.PersistenceProvider;
+import javax.persistence.spi.PersistenceUnitInfo;
+import javax.persistence.spi.ProviderUtil;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class EMFBuilderServiceResolver implements PersistenceProvider, ProviderUtil {
+
+	
+	
+	private final ServiceTracker<Object, Object> tracker;
+	
+	public EMFBuilderServiceResolver(BundleContext context) {
+		tracker = new ServiceTracker<Object, Object>(context, 
+				"org.osgi.service.jpa.EntityManagerFactoryBuilder", null);
+		
+		tracker.open();
+	}
+	
+	public void close() {
+		tracker.close();
+	}
+	
+	/**
+	 * This method looks for a matching EntityManagerFactoryBuilder service to create the
+	 * EMF with.
+	 */
+	@Override
+	public EntityManagerFactory createEntityManagerFactory(String emName, @SuppressWarnings("rawtypes") Map map) {
+		for (Entry<ServiceReference<Object>, Object> e : tracker.getTracked().entrySet()) {
+			String serviceUnitName = String.valueOf(e.getKey().getProperty("osgi.unit.name"));
+			
+			if(serviceUnitName.equals(emName)) {
+				try {
+					Object emfBuilder = e.getValue();
+					Method m = emfBuilder.getClass().getMethod("createEntityManagerFactory", Map.class);
+					return (EntityManagerFactory) m.invoke(emfBuilder, map);
+				} catch (Exception ex) {
+					throw new PersistenceException("Failed to create an EntityManagerFactory for unit " +
+							emName, ex);
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * This method is not intended to be used, as this PersistenceProvider is internal to
+	 * the Spec API for supporting static factory usage (see OSGi JPA spec 127.7.1)
+	 */
+	@Override
+	public EntityManagerFactory createContainerEntityManagerFactory(PersistenceUnitInfo info, @SuppressWarnings("rawtypes") Map map) {
+		return null;
+	}
+
+	@Override
+	public ProviderUtil getProviderUtil() {
+		return this;
+	}
+
+	@Override
+	public LoadState isLoadedWithoutReference(Object entity, String attributeName) {
+		return LoadState.UNKNOWN;
+	}
+
+	@Override
+	public LoadState isLoadedWithReference(Object entity, String attributeName) {
+		return LoadState.UNKNOWN;
+	}
+
+	@Override
+	public LoadState isLoaded(Object entity) {
+		return LoadState.UNKNOWN;
+	}
+
+}
---
+++ javax-persistence/2_1/src/main/java/org/apache/aries/jpa/javax/persistence/AriesJPASpecActivator.java
@@ -0,0 +1,71 @@
+/*  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.aries.jpa.javax.persistence;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.spi.PersistenceProvider;
+import javax.persistence.spi.PersistenceProviderResolver;
+import javax.persistence.spi.PersistenceProviderResolverHolder;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class AriesJPASpecActivator implements BundleActivator, PersistenceProviderResolver {
+
+	private ServiceTracker<PersistenceProvider, PersistenceProvider> tracker;
+	
+	private EMFBuilderServiceResolver emfResolver;
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		emfResolver = new EMFBuilderServiceResolver(context);
+		
+		tracker = new ServiceTracker<PersistenceProvider, PersistenceProvider>(context, 
+				PersistenceProvider.class, null);
+		tracker.open();
+		
+		PersistenceProviderResolverHolder.setPersistenceProviderResolver(this);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		PersistenceProviderResolverHolder.setPersistenceProviderResolver(null);
+		tracker.close();
+		emfResolver.close();
+	}
+
+	@Override
+	public List<PersistenceProvider> getPersistenceProviders() {
+		Collection<PersistenceProvider> services = tracker.getTracked().values();
+		
+		List<PersistenceProvider> providers = new ArrayList<PersistenceProvider>(services.size() + 1);
+		
+		providers.add(emfResolver);
+		providers.addAll(services);
+		
+		return providers;
+	}
+
+	@Override
+	public void clearCachedProviders() {
+		// This is a no-op
+	}
+}
